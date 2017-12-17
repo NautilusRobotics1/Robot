9cc9ed81b1cce8003969d9588f83544a6ccbf6cb
yangc8128 <harpsbro2283@gmail.com>
2017-12-17 15:23:10 -0800

    Master: Removing files for future work, saved in a branch

--- MATE_Challenge_2017/Component.h
+++
@@ -1,9 +0,0 @@
-// The PIN information
-
-class Component {
-  public:
-    Component(int&* pins, int sizeOfPins);
-    int* pins;
-    int sizeOfPins;
-}
-
--- MATE_Challenge_2017/Sensor.h
+++
@@ -1,9 +0,0 @@
-#include "Driver.h"
-
-class Sensor : Driver {
-  public:
-    Sensor(int&* pins, int sizeOfPins) : Driver(int&* pins, int sizeOfPins) { };
-
-    virtual void init(int INTERVAL);
-    virtual void update( );
-}
