3c74a242ac60d7784e340b3fd63a03b581922e4c
yangc8128 <harpsbro2283@gmail.com>
2017-12-23 16:00:16 -0800

    Module_Communication: Updated PiDriver.py's readFrom Arduino with a more descriptive account of the bytes break down being sent from the arduino.

--- MATE_Challenge_2017/PiDriver.py
+++ MATE_Challenge_2017/PiDriver.py
@@ -1,10 +1,14 @@
 import serial
 import socket
 
+# Initializing Socket Communication
 ser = serial.Serial('/dev/ttyACMO',9600, timeout=1)
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+# Main Thread
+# Todo account for SIGTERM (i.e. kill signal) and flush_in_buffer
 while True:
+
 	# Motor Code
     readFromLaptop()
     writeToArduino()
@@ -17,9 +21,22 @@ while True:
 # Baseline from: https://github.com/pyserial/pyserial/issues/216
 # Pi to Laptop from: https://raspberrypi.stackexchange.com/questions/14518/sending-live-data-readings-from-raspberry-pi-to-laptop
 
+# returns bytes
 def readFromArduino():
+	# Example reads
+	# x = ser.read() # Read 1 byte
+	# y = ser.read(10) # Reading 10 bytes
+	# line = ser.readline() # Reading until EOL
 
-	#reading = ser.readline()
+	message =  ser.read(27)
+		# Reading Sensor Data
+	    # Bytes 0-11 are from Accelerometer
+	    # Byte 12 from Pressure
+	    # Byte 13 from Altitude
+	    # Byte 14-25 from Gyroscope
+	    # Bye 26 from Azimuth
+	    # Byte 27 from Temp
+
 	return message;
 
 def writeToArduino():
