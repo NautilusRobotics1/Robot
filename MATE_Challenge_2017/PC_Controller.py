4a0380865c8d09b3d4551c928bd10e85f3d6b1c3
DavidChau89 <32610290+DavidChau89@users.noreply.github.com>
2018-01-07 01:43:17 -0800

    TCP server is now able to send array through the server.
    Todo:
    Code the motors to move according to controller input.

--- MATE_Challenge_2017/PC_Controller.py
+++ MATE_Challenge_2017/PC_Controller.py
@@ -2,15 +2,17 @@
 Controller.py
 David Chau
 
-Based off code from:
+Sources:
 http://robots.dacloughb.com/project-1/logitech-game-pad/
 http://yameb.blogspot.com/2013/01/gamepad-input-in-python.html
+https://docs.python.org/2/library/socket.html#socket-objects
+https://docs.python.org/2/library/pickle.html
 """
 
 import socket               
 import pygame
 import time
-import pickle
+import cPickle
 '''
 Gets joystick data and prints it
 '''
@@ -20,8 +22,19 @@ j.init()
 print 'Initialized Joystick : %s' % j.get_name()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)        
-host = '192.168.137.48' # ip of raspberry pi 
+host = '192.168.137.173' # ip of raspberry pi 
 port = 12345
+
+pi_connection = False
+print 'Connecting...'
+while pi_connection is False:
+    try:
+        s.connect((host, port))
+        pi_connection = True
+    except:
+        pass
+    
+time.sleep(3)
 """
 Returns a vector of the following form:
 [LThumbstickX, LThumbstickY, Unknown Coupled Axis???, 
@@ -43,36 +56,16 @@ def get():
     
     #Read input from the two joysticks       
     for i in range(0, j.get_numaxes()):
-        out[it] = str("{0:.2f}".format(j.get_axis(i)))
+        out[it] = j.get_axis(i)
         it+=1
     #Read input from buttons
     for i in range(0, j.get_numbuttons()):
-        out[it] = str("{0:.2f}".format(j.get_button(i)))
+        out[it] = j.get_button(i)
         it+=1
     return out
 
-try:               
-    s.connect((host, port))
-except:
-    print 'Try connecting again'
-
-def numCov(speed):
-    if(speed[0] != '-'):
-        speed = '+' + speed
-    return speed
-
 while True:    
-    value = str(get()[0:4])
-    #print value
-    C_Input = get()
-    speed1 = numCov(str(C_Input[0]))
-    speed2 = numCov(str(C_Input[1]))
-    s.sendall(speed1)
-    s.sendall(speed2)
-    print speed1, ':', speed2
-    #s.send(str(controller_List[0]))
-    #s.send(str(controller_List[1]))
-    #s.send(str(controller_List[2]))
-    #s.send(value4)
-    time.sleep(0.03) #delay for the raspberry pi server    
-    
+    data = cPickle.dumps(get(),0)
+    s.send(data)
+    time.sleep(0.03) #delay for the raspberry pi server
+
--- MATE_Challenge_2017/RaspPi.py
+++ MATE_Challenge_2017/RaspPi.py
@@ -1,30 +1,60 @@
+'''
+RaspPi.py
+David Chau
+sources:
+https://github.com/nanpy/nanpy-firmware
+'''
+
+from nanpy import (ArduinoApi, SerialManager)
+from nanpy import Servo
 import socket
-import serial
 import time
-import struct
+import cPickle
 
 #ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = '192.168.137.48' #ip of raspberry pi
+host = '192.168.137.173' #ip of raspberry pi
 port = 12345
 s.bind((host, port))
-
 s.listen(5)
 c, addr = s.accept()
 
-def convert(speed):
-  if(speed[0] == '+'):
-    speed = speed[1:5]
-  return speed
+try:
+    connection = SerialManager()
+    ard = ArduinoApi(connection = connection) #auto detect arduino connection
+except:
+    print 'Arduino not connected'
 
+motor_1 = Servo(11) #front left motor
+motor_2 = Servo(10) #front right motor
+motor_3 = Servo(9)  #back left motor
+motor_4 = Servo(6)  #back right motor
+motor_5 = Servo(5)  #top left 
+motor_6 = Servo(3)  #top right
+
+def forword(speedL, speedR):
+    motor_1.writeMicroseconds()
+    motor_2.writeMicroseconds()
+
+def backword(speedL, speedR):
+    motor_3.writeMicroseconds()
+    motor_4.writeMicroseconds()
+   
+def up_down(speedL, speedR):
+    motor_5.writeMicroseconds()
+    motor_6.writeMicroseconds()
+     
+
+def pitch(speedL, speedR):
+    motor_5.writeMicroseconds()
+    motor_6.writeMicroseconds()
+    
 while True:
-  control_LR = c.recv(5)
-  control_UP = c.recv(5)
-  #control_LR2 = getValue()
-  #value = convert(control_LR)
-  #value = control_LR
-  print control_LR, ':', control_UP#, ':', control_LR2
-
-
-
-
+    value = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    data = c.recv(200)
+    try:
+        value = cPickle.loads(data)
+    except:
+        pass
+    print (value[0:4])
+   
