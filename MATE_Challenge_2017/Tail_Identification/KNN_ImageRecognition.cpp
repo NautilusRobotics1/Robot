b4e53f50eaf9f1a7de7e93e80ac919f9c0c4e666
yangc8128 <harpsbro2283@gmail.com>
2018-04-11 05:15:19 -0700

    OpenCV: Working text identifier code, but still has issues with misinterpreting "1" as capital "I".

--- MATE_Challenge_2017/Tail_Identification/KNN_ImageRecognition.cpp
+++ MATE_Challenge_2017/Tail_Identification/KNN_ImageRecognition.cpp
@@ -62,7 +62,7 @@ int main(int argc, char *argv[])
     for(;;)
     {
         //captRefrnc >> frameReference; TODO
-        frameReference = cv::imread("tail_sample3.png");
+        frameReference = cv::imread("tail_sample2.png");
         cv::Mat imgThresh = processFrame(frameReference, ptContours, v4iHierarchy);
 
         // Initialize allContoursWithData w/ boundingRect area
@@ -76,49 +76,75 @@ int main(int argc, char *argv[])
         }
 
         std::vector<int> labels;
-		int r_tol = 2;
+		int r_tol = 3;
         
+		// cwdLeft.boundingRect.x < cwdRight.boundingRect.x
+
         //TODO: Finish clustering
 		// https://stackoverflow.com/questions/33825249/opencv-euclidean-clustering-vs-findcontours/33834092#33834092
 		int n_labels = cv::partition(allContoursWithData, labels, [r_tol](const ContourWithData& lhs, const ContourWithData& rhs) {
-			return std::abs(lhs.fltArea - rhs.fltArea) < r_tol * std::abs(lhs.fltArea - rhs.fltArea);
+			// Filter out by w/h ratio
+			//float temp1 = (lhs.boundingRect.width * 1.0) / lhs.boundingRect.height;
+			//float temp2 = (rhs.boundingRect.width * 1.0)/ rhs.boundingRect.height;
+			//if (!(temp1*1.15 > temp2 && temp1*.65 < temp2)) return false;
+
+			// Filter out by proximity
+			if (!(lhs.boundingRect.x + lhs.boundingRect.width*2 > rhs.boundingRect.x && lhs.boundingRect.x - lhs.boundingRect.width*2 < rhs.boundingRect.x)) return false;
+
+			// Filter out by area
+			//return std::abs(lhs.fltArea - rhs.fltArea) < r_tol * std::abs(lhs.fltArea - rhs.fltArea);
+			if (lhs.fltArea < rhs.fltArea)
+				return rhs.fltArea < lhs.fltArea * r_tol;
+			else
+				return lhs.fltArea < rhs.fltArea * r_tol;
 		});
 
-        std::vector< std::vector<cv::Point> > contoursPartitioned(n_labels);
+        std::vector< std::vector<ContourWithData> > contoursPartitioned(n_labels);
         for (int i = 0; i < allContoursWithData.size(); ++i) {
-			std::vector<cv::Point> p = allContoursWithData[i].ptContour;
-			contoursPartitioned[labels[i]].emplace_back(p); // Formerly an error on an undefined push_back, but fixed with an emplace_back
+			contoursPartitioned[labels[i]].emplace_back(allContoursWithData[i]); // Formerly an error on an undefined push_back, but fixed with an emplace_back
         }
 
         // Filter out invalid contours
 		//  - Condition: in a partition of 3, that are close
 		//	or
 		//  - Condition: in a single partition of 3, with a valid color
-        for (int i = 0; i < allContoursWithData.size(); i++) {
-			// TODO: Check for Shapes Triangle / Trapezoid / Rectangle
-			/*
-			std::vector< std::vector<cv::Point> > approx;
-			approx.resize(validContoursWithData.size());
-			for (int k = 0; k < validContoursWithData.size(); k++) {
-				approxPolyDP(cv::Mat(validContoursWithData[k].ptContour), approx[k], 3, true);
-			}*/
+		bool scannedTextTag = false;
+		// End output, label
+		std::string strFinalString;
+		for (int i = 0; i < contoursPartitioned.size(); i++) {
 			// TODO: Check for Grouped Contours of x3 for Lettering
+			//if (contoursPartitioned[i].size() >= 3 && scannedTextTag == false) {
+				// sort contours from left to right
+				std::sort(contoursPartitioned[i].begin(), contoursPartitioned[i].end(), ContourWithData::sortByBoundingRectXPosition);
+				// Uses KNN and produces a tag inference
+				processContours(kNearest, contoursPartitioned[i], frameReference, imgThresh, strFinalString);
+				// Determines if strFinalString is appropirate
+				if (!strFinalString.empty()) {
+					scannedTextTag == true;
+					break;
+				}
+			//}
 
+			// TODO: Check for Shapes Triangle / Trapezoid / Rectangle
+			if (scannedTextTag) {
+				/*
+				std::vector< std::vector<cv::Point> > approx;
+				approx.resize(validContoursWithData.size());
+				for (int k = 0; k < validContoursWithData.size(); k++) {
+				approxPolyDP(cv::Mat(validContoursWithData[k].ptContour), approx[k], 3, true);
+				}*/
+
+			}
+
+			/*
             if (allContoursWithData[i].checkIfContourIsValid()) {
                 validContoursWithData.push_back(allContoursWithData[i]);
-            }
+            }*/
         }
-		// TODO: Modify and add to each parition
-        // sort contours from left to right
-        std::sort(validContoursWithData.begin(), validContoursWithData.end(), ContourWithData::sortByBoundingRectXPosition);
-
-        // End output, label
-        std::string strFinalString;
-        processContours(kNearest,validContoursWithData,frameReference,imgThresh,strFinalString);
 
         // show final string & input image with green boxes drawn around found digits
         std::cout << "\n\n" << "numbers read = " << strFinalString << "\n\n";
-        imshow(WIN_RF, frameReference); 
+        //imshow(WIN_RF, frameReference); 
         char c = (char)cv::waitKey(20);
         if (c == 27) break;
     }
@@ -127,7 +153,9 @@ int main(int argc, char *argv[])
 
 void processContours(cv::Ptr<cv::ml::KNearest> &_kNearest, std::vector<ContourWithData> &_validContoursWithData, cv::Mat imgSrc, cv::Mat matThresh, std::string &_strFinalString)
 {
+	std::string validSubStrings = "UH8L6RG7CSIPJW3A2X"; // Changed 1 to I because at the moment KNN isn't too accurate 
     for (int i = 0; i < _validContoursWithData.size(); i++) {
+		if (!_validContoursWithData[i].checkIfContourIsValid()) break;
         // draw a green rect around the current char atop original image
         cv::rectangle(imgSrc, _validContoursWithData[i].boundingRect, cv::Scalar(0, 255, 0), 2);
 
@@ -151,5 +179,11 @@ void processContours(cv::Ptr<cv::ml::KNearest> &_kNearest, std::vector<ContourWi
         // append current char to full string
         _strFinalString = _strFinalString + char(int(fltCurrentChar));
     }
+
+	if (_strFinalString.empty()) return;
+
+	// Last check for a valid tag
+	std::size_t found = validSubStrings.find(_strFinalString);
+	if (found == std::string::npos) _strFinalString.clear();
 }
 
