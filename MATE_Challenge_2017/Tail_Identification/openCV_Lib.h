ace3ef5c8cffac5e02e0a95592760a956d051ff4
yangc8128 <harpsbro2283@gmail.com>
2018-04-13 12:14:08 -0700

    OpenCV: Slight change of shared code from RETR_EXTERNAL to RETR_LIST, because the larger images were not picking up on the tags. Additionally the colored shapes are now registering as contours as well.

--- MATE_Challenge_2017/Tail_Identification/openCV_Lib.h
+++ MATE_Challenge_2017/Tail_Identification/openCV_Lib.h
@@ -72,7 +72,8 @@ cv::Mat static processFrame (cv::Mat &imgSrc, std::vector<std::vector<cv::Point>
     // make a copy of the thresh image, this in necessary b/c findContours modifies the image
     imgThreshCopy = imgThresh.clone();
     // loads _ptContours with contours cut out from the imgThreshCopy
-    cv::findContours(imgThreshCopy, _ptContours, _v4iHierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+	// https://stackoverflow.com/questions/44601734/cv2-findcontours-not-able-to-detect-contours
+    cv::findContours(imgThreshCopy, _ptContours, _v4iHierarchy, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE); // RETR_EXTERNAL to RETR_LIST
 
     return imgThresh;
 }
