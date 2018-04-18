3e8e9c0a1e3238a2a2d409caa0c275e2430a5965
yangc8128 <harpsbro2283@gmail.com>
2018-04-17 17:47:50 -0700

    openCV_ColorShapes: Added red hue filtering

--- MATE_Challenge_2017/Tail_Identification/KNN_ImageRecognition.cpp
+++ MATE_Challenge_2017/Tail_Identification/KNN_ImageRecognition.cpp
@@ -14,7 +14,7 @@ class ContourWithData {
         double fltArea;                             // area of contour
         //cv::Moments moment;                         // center of contour
 
-        // TODO: Possibly make more robust
+        // TODO: Possibly make more robust?
         bool checkIfContourIsValid() {
             if (fltArea < MIN_CONTOUR_AREA) return false;
             return true;
@@ -28,126 +28,128 @@ class ContourWithData {
 
 void processContours(cv::Ptr<cv::ml::KNearest> &_kNearest, std::vector<ContourWithData> &_validContoursWithData, cv::Mat imgSrc, cv::Mat matThresh, std::string &_strFinalString);
 
-int main(int argc, char *argv[])
-{
-    /*******************Init Video Feed****************************/
-    //cv::VideoCapture captRefrnc(0); // Using the default camera
-    //if (!captRefrnc.isOpened())
-    //{
-    //    std::cout  << "Could not open reference vide" << std::endl;
-    //    return -1;
-    //}
-    const char* WIN_RF = "Reference";
-
-    /*******************Loading Train Data*************************/
-    cv::Mat matClassificationInts; // read in classifications
-    cv::Mat matTrainingImagesAsFlattenedFloats; // read in matching images(floats)
-    loadTrainingData(matClassificationInts,matTrainingImagesAsFlattenedFloats);
-
-    /*******************Train KNN**********************************/
-    cv::Ptr<cv::ml::KNearest>  kNearest(cv::ml::KNearest::create());
-    kNearest->train(matTrainingImagesAsFlattenedFloats, cv::ml::ROW_SAMPLE, matClassificationInts);
-
-
-	std::vector<std::string> trainingImages = {
-		//"tailA_1.png", "tailA_2.png", "tailA_3.png", "tailA_4.png", "tailA_5.png",
-		//"tailB_1.png", "tailB_2.png", "tailB_3.png", "tailB_4.png",
-		//"tailC_1.png", "tailC_2.png", "tailC_3.png", "tailC_4.png",
-		"tailD_1.png",// "tailD_2.png", "tailD_3.png", "tailD_4.png",
-		//"tailE_1.png", "tailE_2.png", 
-		"tailE_3.png"//, "tailE_4.png",
-		//"tailF_1.png", "tailF_2.png", "tailF_3.png", "tailF_4.png"
-	};
-
-	/*******************Processing Frame***************************/
-	// takes a frame and process it for colors/shapes/alphanumerics
-	for (auto& fName: trainingImages) // was for(;;)
-	{
-		/*******************Prep for Processing************************/
-		std::vector<ContourWithData> allContoursWithData;
-		std::vector<ContourWithData> validContoursWithData;
-		std::vector<std::vector<cv::Point> > ptContours;        // declare contours vector
-		std::vector<cv::Vec4i> v4iHierarchy;                    // declare contours hierarchy
-		cv::Mat frameReference;
-
-        //captRefrnc >> frameReference; TODO
-        frameReference = cv::imread(fName);
-        cv::Mat imgThresh = processFrame(frameReference, ptContours, v4iHierarchy);
-
-        // Initialize allContoursWithData w/ boundingRect area
-        for (int i = 0; i < ptContours.size(); i++) {
-            ContourWithData contourWithData;
-            contourWithData.ptContour = ptContours[i];
-            contourWithData.boundingRect = cv::boundingRect(contourWithData.ptContour);
-            contourWithData.fltArea = cv::contourArea(contourWithData.ptContour);
-            //contourWithData.moment = cv::moments(ptContours[i],false);
-			// Preventing an invalid partition from being processed
-			if (!contourWithData.checkIfContourIsValid()) continue;		// Moved to reduce contours total contours and partitions
-            allContoursWithData.push_back(contourWithData);
-        }
-
-		// Preparing for contour partitions
-        std::vector<int> labels;
-		int r_tol = 3;
-		float h_tol = 0.25;
-        
-		// Designating contour partitions via lambda filter
-		int n_labels = cv::partition(allContoursWithData, labels, [r_tol,h_tol](const ContourWithData& lhs, const ContourWithData& rhs) {
-			// Filter out by hight differentials
-			if (!(std::abs(lhs.boundingRect.height - rhs.boundingRect.height) < lhs.boundingRect.height*h_tol)) return false;
-
-			// Filter out by proximity
-			if (!(lhs.boundingRect.x + lhs.boundingRect.width*2 > rhs.boundingRect.x 
-				&& lhs.boundingRect.x - lhs.boundingRect.width*2 < rhs.boundingRect.x)) return false;
-		});
-
-		// Loading contour partitions
-        std::vector< std::vector<ContourWithData> > contoursPartitioned(n_labels);
-        for (int i = 0; i < allContoursWithData.size(); ++i) {
-			// FROMERLY an ERROR on an undefined push_back, but fixed with an emplace_back
-			contoursPartitioned[labels[i]].emplace_back(allContoursWithData[i]);
-        }
-
-		// Preparing to process contour partitions
-		bool scannedTextTag = false;
-		std::string strFinalString;
-
-		// Process contour partitions
-		for (int i = 0; i < contoursPartitioned.size(); i++) {
-			if (contoursPartitioned[i].size() < 3) continue;
-
-			// Check for Grouped Contours of x3 for Lettering
-			if (scannedTextTag == false) {
-				// sort contours from left to right
-				std::sort(contoursPartitioned[i].begin(), contoursPartitioned[i].end(), ContourWithData::sortByBoundingRectXPosition);
-				// Uses KNN and produces a tag inference
-				processContours(kNearest, contoursPartitioned[i], frameReference, imgThresh, strFinalString);
-				// Determines if strFinalString is appropirate
-				if (!strFinalString.empty()) {
-					scannedTextTag = true;
-					break;
-				}
-			}
-
-			// TODO: Check for Shapes Triangle / Trapezoid / Rectangle
-			if (scannedTextTag) {
-				//std::vector< std::vector<cv::Point> > approx;
-				//approx.resize(validContoursWithData.size());
-				//for (int k = 0; k < validContoursWithData.size(); k++) {
-				//approxPolyDP(cv::Mat(validContoursWithData[k].ptContour), approx[k], 3, true);
-				//}
-
-			}
-        }
-
-        // show final string & input image with green boxes drawn around found digits
-        std::cout << "tag read = " << strFinalString << "\n";
-        cv::imshow(WIN_RF, frameReference);
-        char c = (char)cv::waitKey(20);
-        if (c == 27) break;
-    }
-    return 0;
-}
+//int main(int argc, char *argv[])
+//{
+//    /*******************Init Video Feed****************************/
+//    //cv::VideoCapture captRefrnc(0); // Using the default camera
+//    //if (!captRefrnc.isOpened())
+//    //{
+//    //    std::cout  << "Could not open reference vide" << std::endl;
+//    //    return -1;
+//    //}
+//    const char* WIN_RF = "Reference";
+//
+//    /*******************Loading Train Data*************************/
+//    cv::Mat matClassificationInts; // read in classifications
+//    cv::Mat matTrainingImagesAsFlattenedFloats; // read in matching images(floats)
+//    loadTrainingData(matClassificationInts,matTrainingImagesAsFlattenedFloats);
+//
+//    /*******************Train KNN**********************************/
+//    cv::Ptr<cv::ml::KNearest>  kNearest(cv::ml::KNearest::create());
+//    kNearest->train(matTrainingImagesAsFlattenedFloats, cv::ml::ROW_SAMPLE, matClassificationInts);
+//
+//
+//	std::vector<std::string> trainingImages = {
+//		"tailA_1.png", "tailA_2.png", "tailA_3.png", "tailA_4.png", "tailA_5.png",
+//		"tailB_1.png", "tailB_2.png", "tailB_3.png", "tailB_4.png",
+//		"tailC_1.png", "tailC_2.png", "tailC_3.png", "tailC_4.png",
+//		"tailD_1.png", "tailD_2.png", "tailD_3.png", "tailD_4.png",
+//		"tailE_1.png", "tailE_2.png", "tailE_3.png", "tailE_4.png",
+//		"tailF_1.png", "tailF_2.png", "tailF_3.png", "tailF_4.png"
+//	};
+//
+//	/*******************Processing Frame***************************/
+//	// takes a frame and process it for colors/shapes/alphanumerics
+//	for (auto& fName: trainingImages) // was for(;;)
+//	{
+//		/*******************Prep for Processing************************/
+//		std::vector<ContourWithData> allContoursWithData;
+//		std::vector<ContourWithData> validContoursWithData;
+//		std::vector<std::vector<cv::Point> > ptContours;        // declare contours vector
+//		std::vector<cv::Vec4i> v4iHierarchy;                    // declare contours hierarchy
+//		cv::Mat frameReference;
+//
+//        //captRefrnc >> frameReference; TODO
+//        frameReference = cv::imread(fName);
+//        cv::Mat imgThresh = processFrame(frameReference, ptContours, v4iHierarchy);
+//
+//        // Initialize allContoursWithData w/ boundingRect area, and filter out one's with small area
+//        for (int i = 0; i < ptContours.size(); i++) {
+//            ContourWithData contourWithData;
+//            contourWithData.ptContour = ptContours[i];
+//            contourWithData.boundingRect = cv::boundingRect(contourWithData.ptContour);
+//            contourWithData.fltArea = cv::contourArea(contourWithData.ptContour);
+//            //contourWithData.moment = cv::moments(ptContours[i],false);
+//
+//			if (!contourWithData.checkIfContourIsValid()) continue;
+//            allContoursWithData.push_back(contourWithData);
+//        }
+//
+//		// Preparing for contour partitions
+//        std::vector<int> labels;
+//		int r_tol = 3;
+//		float h_tol = 0.25;
+//        
+//		// Designating contour partitions via lambda filter
+//		int n_labels = cv::partition(allContoursWithData, labels, [r_tol,h_tol](const ContourWithData& lhs, const ContourWithData& rhs) {
+//			// Filter out by hight differentials
+//			if (!(std::abs(lhs.boundingRect.height - rhs.boundingRect.height) < lhs.boundingRect.height*h_tol)) return false;
+//
+//			// Filter out by proximity
+//			if (!(lhs.boundingRect.x + lhs.boundingRect.width*2 > rhs.boundingRect.x 
+//				&& lhs.boundingRect.x - lhs.boundingRect.width*2 < rhs.boundingRect.x)) return false;
+//		});
+//
+//		// Loading contour partitions
+//        std::vector< std::vector<ContourWithData> > contoursPartitioned(n_labels);
+//        for (int i = 0; i < allContoursWithData.size(); ++i) {
+//			// FROMERLY an ERROR on an undefined push_back, but fixed with an emplace_back
+//			contoursPartitioned[labels[i]].emplace_back(allContoursWithData[i]);
+//        }
+//
+//		// Preparing to process contour partitions
+//		bool scannedTextTag = false;
+//		std::string strFinalString;
+//
+//		// Process contour partitions
+//		for (int i = 0; i < contoursPartitioned.size(); i++) {
+//			if (contoursPartitioned[i].size() < 3) continue;
+//
+//			// Check for Grouped Contours of x3 for Lettering
+//			if (scannedTextTag == false) {
+//				// sort contours from left to right
+//				std::sort(contoursPartitioned[i].begin(), contoursPartitioned[i].end(), ContourWithData::sortByBoundingRectXPosition);
+//				// Uses KNN and produces a tag inference
+//				processContours(kNearest, contoursPartitioned[i], frameReference, imgThresh, strFinalString);
+//				// Determines if strFinalString is appropirate
+//				if (!strFinalString.empty()) {
+//					scannedTextTag = true;
+//					break;
+//				}
+//			}
+//
+//			// TODO: Check for Shapes Triangle / Trapezoid / Rectangle
+//			if (scannedTextTag) {
+//				//std::vector< std::vector<cv::Point> > approx;
+//				//approx.resize(validContoursWithData.size());
+//				//for (int k = 0; k < validContoursWithData.size(); k++) {
+//				//approxPolyDP(cv::Mat(validContoursWithData[k].ptContour), approx[k], 3, true);
+//				//  // if approx[k] is 3 then triangle
+//				//  // else if approx[k] is 4 then rectangle / trapezoid
+//				//}
+//
+//			}
+//			// Note is it possible to change the image thresholding filter to look for RBY and then turn to black and white?
+//        }
+//
+//        // show final string & input image with green boxes drawn around found digits
+//        std::cout << "tag read = " << strFinalString << "\n";
+//        cv::imshow(WIN_RF, frameReference);
+//        char c = (char)cv::waitKey(20);
+//        if (c == 27) break;
+//    }
+//    return 0;
+//}
 
 
 void processContours(cv::Ptr<cv::ml::KNearest> &_kNearest, std::vector<ContourWithData> &_validContoursWithData, cv::Mat imgSrc, cv::Mat matThresh, std::string &_strFinalString)
@@ -155,7 +157,6 @@ void processContours(cv::Ptr<cv::ml::KNearest> &_kNearest, std::vector<ContourWi
 	std::string validSubStrings = "UH8L6RG7CS1PJW3A2X";
     for (int i = 0; i < _validContoursWithData.size(); i++) {
 		// Preventing an invalid partition from being processed
-		//if (!_validContoursWithData[i].checkIfContourIsValid()) break; // TODO moved to init of allcontourswithdata
 
         // draw a green rect around the current char atop original image
         cv::rectangle(imgSrc, _validContoursWithData[i].boundingRect, cv::Scalar(0, 255, 0), 2);
--- MATE_Challenge_2017/Tail_Identification/openCV_Lib.h
+++ MATE_Challenge_2017/Tail_Identification/openCV_Lib.h
@@ -61,19 +61,45 @@ cv::Mat static processFrame (cv::Mat &imgSrc, std::vector<std::vector<cv::Point>
         std::exit(-1);
     }
 
-    // convert to grayscale
-    cv::cvtColor(imgSrc, imgGrayscale, CV_BGR2GRAY);
-    // blurring
-    cv::GaussianBlur(imgGrayscale, imgBlurred, cv::Size(5, 5), 0);
-    // filter image from grayscale to black and white
-    cv::adaptiveThreshold(imgBlurred, imgThresh, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY_INV, 11, 2);
-    // show threshold image for reference
-    cv::imshow("imgThresh", imgThresh);
-    // make a copy of the thresh image, this in necessary b/c findContours modifies the image
-    imgThreshCopy = imgThresh.clone();
-    // loads _ptContours with contours cut out from the imgThreshCopy
-	// https://stackoverflow.com/questions/44601734/cv2-findcontours-not-able-to-detect-contours
-    cv::findContours(imgThreshCopy, _ptContours, _v4iHierarchy, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE); // RETR_EXTERNAL to RETR_LIST
+	// Clear for Blue triangle, but now there is a triangle from the table too
+	int low_r = 0, low_g = 0, low_b = 0;
+	int high_r = 80, high_g = 80, high_b = 255;
+	//int low_r = 0, low_g = 0, low_b = 0;
+	//int high_r = 50, high_g = 50, high_b = 255;
+
+	// https://docs.opencv.org/3.4.0/d7/d4d/tutorial_py_thresholding.html
+	// https://stackoverflow.com/questions/12204522/efficiently-threshold-red-using-hsv-in-opencv
+	//cv::cvtColor(imgSrc, imgRGB, CV_RGB2HSV); // Isn't really grayscale though
+	cv::inRange(imgSrc, cv::Scalar(low_b, low_g, low_r), cv::Scalar(high_b, high_g, high_r), imgThresh);
+	cv::imshow("imgThresh", imgThresh);
+	// convert to grayscale
+	//cv::cvtColor(imgThresh, imgGrayscale, CV_BGR2GRAY); // Isn't really grayscale though
+	//cv::imshow("imgGrayscale", imgGrayscale);
+
+	// blurring
+	//cv::GaussianBlur(imgGrayscale, imgBlurred, cv::Size(5, 5), 0);
+	// filter image from grayscale to black and white
+	//cv::adaptiveThreshold(imgBlurred, imgThresh, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY_INV, 11, 2);
+	// show threshold image for reference
+	//cv::imshow("imgThresh", imgThresh);
+	// make a copy of the thresh image, this in necessary b/c findContours modifies the image
+	imgThreshCopy = imgThresh.clone();
+	// loads _ptContours with contours cut out from the imgThreshCopy
+	cv::findContours(imgThreshCopy, _ptContours, _v4iHierarchy, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE); // RETR_EXTERNAL to RETR_LIST
+
+ //   // convert to grayscale
+ //   cv::cvtColor(imgSrc, imgGrayscale, CV_BGR2GRAY);
+ //   // blurring
+ //   cv::GaussianBlur(imgGrayscale, imgBlurred, cv::Size(5, 5), 0);
+ //   // filter image from grayscale to black and white
+ //   cv::adaptiveThreshold(imgBlurred, imgThresh, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY_INV, 11, 2);
+ //   // show threshold image for reference
+ //   cv::imshow("imgThresh", imgThresh);
+ //   // make a copy of the thresh image, this in necessary b/c findContours modifies the image
+ //   imgThreshCopy = imgThresh.clone();
+ //   // loads _ptContours with contours cut out from the imgThreshCopy
+	//// https://stackoverflow.com/questions/44601734/cv2-findcontours-not-able-to-detect-contours
+ //   cv::findContours(imgThreshCopy, _ptContours, _v4iHierarchy, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE); // RETR_EXTERNAL to RETR_LIST
 
     return imgThresh;
 }
