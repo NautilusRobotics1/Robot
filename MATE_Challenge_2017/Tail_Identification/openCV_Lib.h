c3a50b8f0a0c0f88d2010021e4e75f79adb09e08
yangc8128 <harpsbro2283@gmail.com>
2018-04-28 09:49:17 -0700

    OpenCV_Shapes: Additional frame processing included an alternative thesholding method specifically for blue.

--- MATE_Challenge_2017/Tail_Identification/openCV_Lib.h
+++ MATE_Challenge_2017/Tail_Identification/openCV_Lib.h
@@ -73,62 +73,7 @@ cv::Mat static processFrame (cv::Mat &imgSrc, std::vector<std::vector<cv::Point>
 	cv::findContours(imgBlurred.clone(), _ptContours, _v4iHierarchy, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE); // RETR_EXTERNAL to RETR_LIST																										  // Fill holes in each contour
 	cv::drawContours(imgBlurred, _ptContours, -1, CV_RGB(255, 255, 255), -1);
 	cv::imshow("found contours", imgBlurred);
-	/*
-	// Set up the parameters (check the defaults in opencv's code in blobdetector.cpp)
-	cv::SimpleBlobDetector::Params params;
-	params.minDistBetweenBlobs = 0.0f;//50.0f;
-	params.filterByInertia = false;
-	params.filterByConvexity = false;
-	params.filterByColor = false;
-	params.filterByCircularity = false;
-	params.filterByArea = true;
-	params.minArea = 20.0f;
-	params.maxArea = 1000000000000000000000.0f;
-	//params.maxArea = 500.0f; // Able to detect just the characters
-	// ... any other params you don't want default value
 
-	// set up and create the detector using the parameters
-	cv::Ptr<cv::SimpleBlobDetector> detector = cv::SimpleBlobDetector::create(params);
-	std::vector<cv::KeyPoint> keypoints;
-	cv::Mat im_with_keypoints;
-
-	// Detecting Blobs
-	detector->detect(imgGrayscale, keypoints);
-	cv::drawKeypoints(imgSrc, keypoints, im_with_keypoints, cv::Scalar(0, 0, 255), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
-	cv::imshow("keypoints", im_with_keypoints);
-	*/
 	cv::waitKey(0);
-/*
-  // Clear for Blue triangle, but now there is a triangle from the table too
-  int low_r = 0, low_g = 0, low_b = 0;
-  int high_r = 80, high_g = 80, high_b = 255;
-  // blurring
-  cv::GaussianBlur(imgSrc, imgBlurred, cv::Size(5, 5), 0);
-  cv::imshow("imgBlurred", imgBlurred);
-
-  // https://docs.opencv.org/3.4.0/d7/d4d/tutorial_py_thresholding.html
-  // https://stackoverflow.com/questions/12204522/efficiently-threshold-red-using-hsv-in-opencv
-  //cv::cvtColor(imgSrc, imgRGB, CV_RGB2HSV); // Isn't really grayscale though
-  cv::inRange(imgBlurred, cv::Scalar(low_b, low_g, low_r), cv::Scalar(high_b, high_g, high_r), imgThresh);
-  cv::imshow("imgThresh", imgThresh);
-
-  imgThreshCopy = imgThresh.clone();
-  cv::findContours(imgThreshCopy, _ptContours, _v4iHierarchy, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE); // RETR_EXTERNAL to RETR_LIST
-*/
-  /*
-    // convert to grayscale
-    cv::cvtColor(imgSrc, imgGrayscale, CV_BGR2GRAY);
-    // blurring
-    cv::GaussianBlur(imgGrayscale, imgBlurred, cv::Size(5, 5), 0);
-    // filter image from grayscale to black and white
-    cv::adaptiveThreshold(imgBlurred, imgThresh, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY_INV, 11, 2);
-    // show threshold image for reference
-    cv::imshow("imgThresh", imgThresh);
-    // make a copy of the thresh image, this in necessary b/c findContours modifies the image
-    imgThreshCopy = imgThresh.clone();
-    // loads _ptContours with contours cut out from the imgThreshCopy
-  // https://stackoverflow.com/questions/44601734/cv2-findcontours-not-able-to-detect-contours
-    cv::findContours(imgThreshCopy, _ptContours, _v4iHierarchy, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE); // RETR_EXTERNAL to RETR_LIST
-  */
-    return imgThresh;
+	return imgThresh;
 }
