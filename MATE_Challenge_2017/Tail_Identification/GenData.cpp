ee9bbca3ddfa5e3d2fecc304bf48ff3b93b440dc
yangc8128 <harpsbro2283@gmail.com>
2018-04-28 09:45:25 -0700

    OpenCV_Shapes: Three frame processing methods are present in the opecv_lib. One simple, sharpen then find contours. Two, sharpen then find blobs. Three, simple greyscale and thresholding then find contours. The uncommented is the first, which only detects the letters as contours for now.

--- MATE_Challenge_2017/Tail_Identification/GenData.cpp
+++ MATE_Challenge_2017/Tail_Identification/GenData.cpp
@@ -19,72 +19,72 @@ int main() {
     // Needs ValidShapes {Triangle, Trapezoid, Rectangle};
     // Needs ValidColor {Red,Blue,Yellow};
 
-	std::vector<std::string> trainingImages = {
-		//"tailA_1.png", "tailA_2.png", "tailA_3.png", "tailA_4.png",
-		//"tailB_1.png", "tailB_2.png", "tailB_3.png", "tailB_4.png",
-		//"tailC_1.png"//, "tailC_2.png", "tailC_3.png", "tailC_4.png",
-		//"tailD_1.png", "tailD_2.png", "tailD_3.png", "tailD_4.png",
-		//"tailE_1.png", "tailE_2.png", "tailE_3.png", "tailE_4.png",
-		"tailF_1.png"//, "tailF_2.png", "tailF_3.png", "tailF_4.png",
-		//"training_chars.png"
-	};
+  std::vector<std::string> trainingImages = {
+    //"tailA_1.png", "tailA_2.png", "tailA_3.png", "tailA_4.png",
+    //"tailB_1.png", "tailB_2.png", "tailB_3.png", "tailB_4.png",
+    //"tailC_1.png"//, "tailC_2.png", "tailC_3.png", "tailC_4.png",
+    //"tailD_1.png", "tailD_2.png", "tailD_3.png", "tailD_4.png",
+    //"tailE_1.png", "tailE_2.png", "tailE_3.png", "tailE_4.png",
+    "tailF_1.png"//, "tailF_2.png", "tailF_3.png", "tailF_4.png",
+    //"training_chars.png"
+  };
 
-	// Training KNN
-	for (auto &fName : trainingImages) // access by reference to avoid copying
-	{
-		imgTrainingNumbers = cv::imread(fName);
-		cv::imshow("imgTrainingNumbers", imgTrainingNumbers);
-		cv::Mat imgThresh = processFrame(imgTrainingNumbers, ptContours, v4iHierarchy);
+  // Training KNN
+  for (auto &fName : trainingImages) // access by reference to avoid copying
+  {
+    imgTrainingNumbers = cv::imread(fName);
+    cv::imshow("imgTrainingNumbers", imgTrainingNumbers);
+    cv::Mat imgThresh = processFrame(imgTrainingNumbers, ptContours, v4iHierarchy);
 
-		// for each contour
-		for (int i = 0; i < ptContours.size(); i++) {
-			// if contour is big enough to consider
-			if (cv::contourArea(ptContours[i]) < MIN_CONTOUR_AREA)
-				continue;
+    // for each contour
+    for (int i = 0; i < ptContours.size(); i++) {
+      // if contour is big enough to consider
+      if (cv::contourArea(ptContours[i]) < MIN_CONTOUR_AREA)
+        continue;
 
-			// get the bounding rect
-			cv::Rect boundingRect = cv::boundingRect(ptContours[i]);
-			// draw red rectangle around each contour as we ask user for input
-			cv::rectangle(imgTrainingNumbers, boundingRect, cv::Scalar(0, 0, 255), 2);
-			// get ROI image of bounding rect
-			cv::Mat matROI = imgThresh(boundingRect);
+      // get the bounding rect
+      cv::Rect boundingRect = cv::boundingRect(ptContours[i]);
+      // draw red rectangle around each contour as we ask user for input
+      cv::rectangle(imgTrainingNumbers, boundingRect, cv::Scalar(0, 0, 255), 2);
+      // get ROI image of bounding rect
+      cv::Mat matROI = imgThresh(boundingRect);
 
-			// resize image, this will be more consistent for recognition and storage
-			cv::Mat matROIResized;
-			cv::resize(matROI, matROIResized, cv::Size(RESIZED_IMAGE_WIDTH, RESIZED_IMAGE_HEIGHT));
+      // resize image, this will be more consistent for recognition and storage
+      cv::Mat matROIResized;
+      cv::resize(matROI, matROIResized, cv::Size(RESIZED_IMAGE_WIDTH, RESIZED_IMAGE_HEIGHT));
 
-			cv::imshow("matROI", matROI);
-			cv::imshow("matROIResized", matROIResized);
-			// show training numbers image, this will now have red rectangles drawn on it
-			cv::imshow("imgTrainingNumbers", imgTrainingNumbers);
+      cv::imshow("matROI", matROI);
+      cv::imshow("matROIResized", matROIResized);
+      // show training numbers image, this will now have red rectangles drawn on it
+      cv::imshow("imgTrainingNumbers", imgTrainingNumbers);
 
-			// get key press
-			int intChar = cv::waitKey(0);
+      // get key press
+      int intChar = cv::waitKey(0);
 
-			// if esc key == 27 was pressed, exit program or if . key == 47 was pressed skip contour
-			if (intChar == 27) return(0);
-			else if (intChar == 46) continue;
-			else if (std::find(intValidChars.begin(), intValidChars.end(), intChar) != intValidChars.end()) {
-				// else if the char is in the list of chars we are looking for . . .
-				
-				// append classification char to integer list of chars
-				matClassificationInts.push_back(intChar);
+      // if esc key == 27 was pressed, exit program or if . key == 47 was pressed skip contour
+      if (intChar == 27) return(0);
+      else if (intChar == 46) continue;
+      else if (std::find(intValidChars.begin(), intValidChars.end(), intChar) != intValidChars.end()) {
+        // else if the char is in the list of chars we are looking for . . .
+        
+        // append classification char to integer list of chars
+        matClassificationInts.push_back(intChar);
 
-				// now add the training image (some conversion is necessary first) . . .
-				cv::Mat matImageFloat;
-				// convert Mat to float
-				matROIResized.convertTo(matImageFloat, CV_32FC1);
+        // now add the training image (some conversion is necessary first) . . .
+        cv::Mat matImageFloat;
+        // convert Mat to float
+        matROIResized.convertTo(matImageFloat, CV_32FC1);
 
-				// flatten
-				cv::Mat matImageFlattenedFloat = matImageFloat.reshape(1, 1);
+        // flatten
+        cv::Mat matImageFlattenedFloat = matImageFloat.reshape(1, 1);
 
-				// add to Mat as though it was a vector, this is necessary due to the data types that KNearest.train accepts
-				matTrainingImagesAsFlattenedFloats.push_back(matImageFlattenedFloat);
-			}
-		}
-	}
+        // add to Mat as though it was a vector, this is necessary due to the data types that KNearest.train accepts
+        matTrainingImagesAsFlattenedFloats.push_back(matImageFlattenedFloat);
+      }
+    }
+  }
 
-	// Finish Up
+  // Finish Up
     std::cout << "training complete\n\n";
     //uploadTrainingData(matClassificationInts, matTrainingImagesAsFlattenedFloats);
     return 0;
--- MATE_Challenge_2017/Tail_Identification/openCV_Lib.h
+++ MATE_Challenge_2017/Tail_Identification/openCV_Lib.h
@@ -6,6 +6,7 @@
 #include <opencv2/imgproc.hpp>  // Gaussian Blur
 #include <opencv2/highgui.hpp>  // OpenCV window I/O
 #include <opencv2/ml/ml.hpp>
+#include <opencv2\features2d.hpp> // For simple blob detection
 
 // global variables
 const int MIN_CONTOUR_AREA = 100;
@@ -60,46 +61,74 @@ cv::Mat static processFrame (cv::Mat &imgSrc, std::vector<std::vector<cv::Point>
         std::cout << "error: image not read\n\n";
         std::exit(-1);
     }
-
-	// Clear for Blue triangle, but now there is a triangle from the table too
-	int low_r = 0, low_g = 0, low_b = 0;
-	int high_r = 80, high_g = 80, high_b = 255;
-	//int low_r = 0, low_g = 0, low_b = 0;
-	//int high_r = 50, high_g = 50, high_b = 255;
-
-	// https://docs.opencv.org/3.4.0/d7/d4d/tutorial_py_thresholding.html
-	// https://stackoverflow.com/questions/12204522/efficiently-threshold-red-using-hsv-in-opencv
-	//cv::cvtColor(imgSrc, imgRGB, CV_RGB2HSV); // Isn't really grayscale though
-	cv::inRange(imgSrc, cv::Scalar(low_b, low_g, low_r), cv::Scalar(high_b, high_g, high_r), imgThresh);
-	cv::imshow("imgThresh", imgThresh);
 	// convert to grayscale
-	//cv::cvtColor(imgThresh, imgGrayscale, CV_BGR2GRAY); // Isn't really grayscale though
-	//cv::imshow("imgGrayscale", imgGrayscale);
+	cv::cvtColor(imgSrc, imgGrayscale, CV_BGR2GRAY);
+	// Blurring
+	cv::GaussianBlur(imgGrayscale, imgBlurred, cv::Size(0, 0), 3);
+	cv::imshow("imgBlurred", imgBlurred);
+	// Sharpening
+	cv::addWeighted(imgGrayscale, 1.5, imgBlurred, -0.5, 0, imgBlurred);
+	cv::imshow("imgSharpened", imgBlurred);
+	
+	cv::findContours(imgBlurred.clone(), _ptContours, _v4iHierarchy, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE); // RETR_EXTERNAL to RETR_LIST																										  // Fill holes in each contour
+	cv::drawContours(imgBlurred, _ptContours, -1, CV_RGB(255, 255, 255), -1);
+	cv::imshow("found contours", imgBlurred);
+	/*
+	// Set up the parameters (check the defaults in opencv's code in blobdetector.cpp)
+	cv::SimpleBlobDetector::Params params;
+	params.minDistBetweenBlobs = 0.0f;//50.0f;
+	params.filterByInertia = false;
+	params.filterByConvexity = false;
+	params.filterByColor = false;
+	params.filterByCircularity = false;
+	params.filterByArea = true;
+	params.minArea = 20.0f;
+	params.maxArea = 1000000000000000000000.0f;
+	//params.maxArea = 500.0f; // Able to detect just the characters
+	// ... any other params you don't want default value
 
-	// blurring
-	//cv::GaussianBlur(imgGrayscale, imgBlurred, cv::Size(5, 5), 0);
-	// filter image from grayscale to black and white
-	//cv::adaptiveThreshold(imgBlurred, imgThresh, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY_INV, 11, 2);
-	// show threshold image for reference
-	//cv::imshow("imgThresh", imgThresh);
-	// make a copy of the thresh image, this in necessary b/c findContours modifies the image
-	imgThreshCopy = imgThresh.clone();
-	// loads _ptContours with contours cut out from the imgThreshCopy
-	cv::findContours(imgThreshCopy, _ptContours, _v4iHierarchy, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE); // RETR_EXTERNAL to RETR_LIST
+	// set up and create the detector using the parameters
+	cv::Ptr<cv::SimpleBlobDetector> detector = cv::SimpleBlobDetector::create(params);
+	std::vector<cv::KeyPoint> keypoints;
+	cv::Mat im_with_keypoints;
 
- //   // convert to grayscale
- //   cv::cvtColor(imgSrc, imgGrayscale, CV_BGR2GRAY);
- //   // blurring
- //   cv::GaussianBlur(imgGrayscale, imgBlurred, cv::Size(5, 5), 0);
- //   // filter image from grayscale to black and white
- //   cv::adaptiveThreshold(imgBlurred, imgThresh, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY_INV, 11, 2);
- //   // show threshold image for reference
- //   cv::imshow("imgThresh", imgThresh);
- //   // make a copy of the thresh image, this in necessary b/c findContours modifies the image
- //   imgThreshCopy = imgThresh.clone();
- //   // loads _ptContours with contours cut out from the imgThreshCopy
-	//// https://stackoverflow.com/questions/44601734/cv2-findcontours-not-able-to-detect-contours
- //   cv::findContours(imgThreshCopy, _ptContours, _v4iHierarchy, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE); // RETR_EXTERNAL to RETR_LIST
+	// Detecting Blobs
+	detector->detect(imgGrayscale, keypoints);
+	cv::drawKeypoints(imgSrc, keypoints, im_with_keypoints, cv::Scalar(0, 0, 255), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+	cv::imshow("keypoints", im_with_keypoints);
+	*/
+	cv::waitKey(0);
+/*
+  // Clear for Blue triangle, but now there is a triangle from the table too
+  int low_r = 0, low_g = 0, low_b = 0;
+  int high_r = 80, high_g = 80, high_b = 255;
+  // blurring
+  cv::GaussianBlur(imgSrc, imgBlurred, cv::Size(5, 5), 0);
+  cv::imshow("imgBlurred", imgBlurred);
 
+  // https://docs.opencv.org/3.4.0/d7/d4d/tutorial_py_thresholding.html
+  // https://stackoverflow.com/questions/12204522/efficiently-threshold-red-using-hsv-in-opencv
+  //cv::cvtColor(imgSrc, imgRGB, CV_RGB2HSV); // Isn't really grayscale though
+  cv::inRange(imgBlurred, cv::Scalar(low_b, low_g, low_r), cv::Scalar(high_b, high_g, high_r), imgThresh);
+  cv::imshow("imgThresh", imgThresh);
+
+  imgThreshCopy = imgThresh.clone();
+  cv::findContours(imgThreshCopy, _ptContours, _v4iHierarchy, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE); // RETR_EXTERNAL to RETR_LIST
+*/
+  /*
+    // convert to grayscale
+    cv::cvtColor(imgSrc, imgGrayscale, CV_BGR2GRAY);
+    // blurring
+    cv::GaussianBlur(imgGrayscale, imgBlurred, cv::Size(5, 5), 0);
+    // filter image from grayscale to black and white
+    cv::adaptiveThreshold(imgBlurred, imgThresh, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY_INV, 11, 2);
+    // show threshold image for reference
+    cv::imshow("imgThresh", imgThresh);
+    // make a copy of the thresh image, this in necessary b/c findContours modifies the image
+    imgThreshCopy = imgThresh.clone();
+    // loads _ptContours with contours cut out from the imgThreshCopy
+  // https://stackoverflow.com/questions/44601734/cv2-findcontours-not-able-to-detect-contours
+    cv::findContours(imgThreshCopy, _ptContours, _v4iHierarchy, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE); // RETR_EXTERNAL to RETR_LIST
+  */
     return imgThresh;
 }
