c9c5562766f1160e06d9d592e470fcde00f9b0a3
yangc8128 <harpsbro2283@gmail.com>
2018-05-04 16:29:56 -0700

    OpenCV_ColorShape: Added sharpening

--- MATE_Challenge_2017/Tail_Identification/openCV_Lib.h
+++ MATE_Challenge_2017/Tail_Identification/openCV_Lib.h
@@ -67,12 +67,17 @@ cv::Mat static processFrame (cv::Mat &imgSrc, std::vector<std::vector<cv::Point>
 	cv::GaussianBlur(imgGrayscale, imgBlurred, cv::Size(0, 0), 3);
 	cv::imshow("imgBlurred", imgBlurred);
 	// Sharpening
-	cv::addWeighted(imgGrayscale, 1.5, imgBlurred, -0.5, 0, imgBlurred);
+	//cv::addWeighted(imgGrayscale, 1.5, imgBlurred, -0.5, 0, imgBlurred); // Given parameters
+	cv::addWeighted(imgGrayscale, 2, imgBlurred, -1.5, 0, imgBlurred);
 	cv::imshow("imgSharpened", imgBlurred);
 	
 	cv::findContours(imgBlurred.clone(), _ptContours, _v4iHierarchy, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE); // RETR_EXTERNAL to RETR_LIST																										  // Fill holes in each contour
 	cv::drawContours(imgBlurred, _ptContours, -1, CV_RGB(255, 255, 255), -1);
 	cv::imshow("found contours", imgBlurred);
+
+	// TODO: Connect Contours
+	// https://stackoverflow.com/questions/19123165/join-close-enough-contours-in-opencv
+	// https://docs.opencv.org/2.4/doc/tutorials/imgproc/erosion_dilatation/erosion_dilatation.html
 
 	cv::waitKey(0);
 	return imgThresh;
